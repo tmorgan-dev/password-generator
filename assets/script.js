78b5b366b233cd4386b9610fc1d2cc01601eb29f
[Tim Morgan] <[tmorgan.dev.2023@gmail.com]>
2023-07-07 10:36:25 -0600

    added logic to verify all desired characters are present

--- assets/script.js
+++ assets/script.js
@@ -5,32 +5,26 @@ var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate")
 
 // Write password to the #password input
-
 function generatePassword() {
-  ;
+
   // password length must be between 8 and 128 char
-  var passwordLength = window.prompt("Enter desired password length, between 8 and 128");
+  var passwordLength = window.prompt("Enter desired password length, between 8 and 128")
   if ((passwordLength < 7) || (passwordLength > 129)) {
-    ;
-    alert("Invalid input; Please try again");
-    return;
-  };
+    alert("Invalid input Please try again")
+    return
+  }
 
   // prompt user weather an upper case character is required
-  var upperRequired = window.confirm("If upper case character is required, click OK. If not click Cancel.");
-
+  var upperRequired = window.confirm("If upper case character is required, click OK. If not click Cancel.")
   // prompt user weather a lower case character is required
-  var lowerRequired = window.confirm("If lower case character is required, click OK. If not click Cancel.");
-
+  var lowerRequired = window.confirm("If lower case character is required, click OK. If not click Cancel.")
   // prompt user weather a number is required
   var numberRequired = window.confirm("If number is required, click OK. If not click Cancel.")
-
   // prompt user weather a special character is required
   var specialRequired = window.confirm("If special character is required, click OK. If not click Cancel.")
-
   if (!upperRequired && !lowerRequired && !numberRequired && !specialRequired) {
     alert("Must select at least one option")
   }
@@ -85,31 +79,34 @@ function generatePassword() {
     var randomElement = array[randomIndex]
     return randomElement
   }
+
   //gets items from the possible password list for the desired password length
   for (var i = 0; i < passwordOptions.passwordLength; i++) {
     var randomPassword = getRandomArray(possiblePassword)
     password.push(randomPassword)
   }
 
-  console.log(possiblePassword)
-
-  if (randomPassword !== includes(passwordCharacters)) {
-    getRandomArray()
+  //final check to force required characters into password in case random selection failed to grab all the required items
+  if (upperRequired === true) {
+    password[0] = getRandomArray(upperCase)
   }
-  else {
-    return password.join("")
+  if (lowerRequired === true) {
+    password[1] = getRandomArray(lowerCase)
   }
-
-  // return password.join("")
+  if (numberRequired === true) {
+    password[2] = getRandomArray(number)
+  }
+  if (specialRequired === true) {
+    password[3] = getRandomArray(special)
+  }
+  return password.join("")
 }
 
 function writePassword() {
-  ;
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-};
+  var password = generatePassword()
+  var passwordText = document.querySelector("#password")
+  passwordText.value = password
+}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword)
